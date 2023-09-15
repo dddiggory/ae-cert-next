fdd1b4bffba518b07020f416a4ca432066b82292
diggory-rycroft <diggory.rycroft@vercel.com>
2023-09-15 13:37:17 -0400

    rename shad components

--- pages/testComponent.js
+++ pages/testComponent.js
@@ -1,7 +1,11 @@
-import React from 'react';
-
-function TestComponent() {
-    return <h1>Hello, Next.js!</h1>;
+/**
+ * v0 by Vercel Labs.
+ * @see https://v0.dev/t/C3Xr3Xz
+ */
+export default function Component() {
+    return (
+      <div
+      className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 animate-pulse glow-lg w-full h-16 rounded"
+    >Welcome</div>
+  )
 }
-
-export default TestComponent;
